1c6c4110effb32434173a9ecc82d71e1e49bbcf5
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-12-20 18:01:51 +0100

    Fine Layout Header

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,10 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-size: 100%;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <div class="header-content">
+            <img src="../assets/img/logo_large.png" alt="">
+            <form>
+                <input type="text" placeholder="Cerca qualcosa da guardare">
+            </form>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -11,5 +16,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../assets/style/variables.scss';
 
+    header {
+        background-color: #000;
+        .header-content {
+            width: $widthContent;
+            height: 120px;
+            margin: $marginConten;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            // Stile componenti di .header-content
+            img {
+                width: 175px;
+            }
+            input {
+                line-height: 2;
+                width: 250px;
+                padding: 0 7px;
+                border: none;
+                border-radius: 7px;
+                background-color: #303030;
+                color: white;
+            }
+        }
+    }
 </style>
