336540168e58dceb80966ff1b562720e58365536
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-12-22 18:07:07 +0100

    Manca solo la milestone 2

--- src/components/elements/Cards.vue
+++ src/components/elements/Cards.vue
@@ -1,31 +1,36 @@
 <template>
     <div class="card">
         <div class="card-content">
-            <div>
-                <span class="titolo">Titolo: </span>
-                <span>{{film.title}}</span>
+            <div class="generali">
+                <div>
+                    <span class="titolo">Titolo: </span>
+                    <span>{{film.title}}</span>
+                </div>
+                <div>
+                    <span class="titolo-originale">Titolo originale: </span>
+                    <span>{{film.original_title}}</span>
+                </div>
+                <div>
+                    <!-- <span v-if="!film.vote_average">*VOTO NON DISPONIBILE*</span> -->
+                    <span v-if="film.vote_average" class="voto">Voto: 
+                        <span v-for="i in 5" :key="i">
+                            <i v-if="i <= getFillStars(film.vote_average)" class="fas fa-star yellowStar"></i>
+                            <i v-else class="far fa-star"></i>
+                        </span>
+                    </span>
+                </div>
             </div>
-            <div>
-                <span class="titolo-originale">Titolo originale: </span>
-                <span>{{film.original_title}}</span>
+            <div class="descrizione" style="overflow-y: auto; height: calc(60% - 12px); margin-top: 12px">
+                <span v-if="film.overview">Descrizione: <span style="font-weight: normal">{{film.overview}}</span></span>
+                <span v-else style="display: inline-block; text-align: center;">*DESCRIZIONE NON DISPONIBILE*</span>
             </div>
-            <div>
-                <span class="voto">Voto: </span>
-                <!-- PROVVISORIO -->
-                <span v-for="i in 5" :key="i">
-                <i v-if="i <= getFillStars(6)" class="fas fa-star yellowStar"></i>
-                <i v-else class="far fa-star"></i>
-                </span>
-                <!--  / PROVVISORIO -->
-            </div>
-            <div>
-                <span class="Descrizione">Descrizione: </span>
-                <span class="testo-descizione">{{film.overview}}</span>
-            </div>
-            </div>
+        </div>
 
         <div class="card-image">
-            <img src='https://picsum.photos/300/400'>
+            <img v-if="film.poster_path" :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`">
+            <div v-else class="no-poster">
+                <span>poster non disponibile</span>
+            </div>
         </div>
     </div>
 </template>
@@ -46,7 +51,10 @@
 
 <style lang="scss" scoped>
     @import '../../assets/style/variables.scss';
- 
+    
+    span {
+        pointer-events: none;
+    }
     .card {
         position: relative;
         width: calc(100% / 4 - 40px);
@@ -64,28 +72,33 @@
             height: 100%;
             background-color: #000;
             padding: 25px 10px;
-            div {
-                margin-top: 9px;
-                span:first-child {
-                font-weight: bold;
-                .testo-descizione {
-                    height: 30%;
-                    overflow-y: auto;
-                }
+            .generali {
+                overflow-y: auto;
+                max-height: 40%;
+                min-height: 40%;
             }
-                span {
-                    color: #fff;
-                    i {
-                        color: #434343;
-                    }
-                    .yellowStar {
-                        color: #ffbd00;
-                    }
-                }
+            .generali div:first-child{
+                margin-top: 0;
+            }
+            .descizione > div, .generali > div {
+                margin-top: 9px;
+                
             }
             &:hover {
                 opacity: 1;
             }
+            span:first-child {
+                font-weight: bold;
+                }
+            span {
+                color: #fff;
+                i {
+                    color: #434343;
+                }
+                .yellowStar {
+                    color: #ffbd00;
+                }
+            } 
         }
         .card-image {
             width: 100%;
@@ -94,6 +107,21 @@
                 width: 100%;
                 height: 100%;
                 display: block;
+            }
+            .no-poster {
+                background-color: black; 
+                height: 100%; 
+                width: 100%; 
+                display: flex; 
+                align-items: center; 
+                justify-content: center; 
+                color: white;
+                span {
+                    display: block;
+                    text-align: center;
+                    font-size: 25px;
+                    text-transform: uppercase;
+                }
             }
         }
     }
